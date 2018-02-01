cfc0b74dd4a38134acad4d0eab659dea06e1b297
William <williamboston123@hotmail.com>
2018-01-31 18:23:09 -0800

    Edit: removed contetns of com_academic
    removed content because i think we need to extend from com_people instead of copying from

--- packages/UserTypes/src/components/com_academic/emails/account_activate.php
+++
@@ -1,4 +0,0 @@
-<?php defined('KOOWA') or die; ?>
-
-<?= sprintf(@text('COM-ACADEMIC-MAIL-BODY-ACCOUNT-ACTIVATE'), $person->name)?> 
-<?= @route('option=com_people&view=session&token='.$person->activationCode) ?>
