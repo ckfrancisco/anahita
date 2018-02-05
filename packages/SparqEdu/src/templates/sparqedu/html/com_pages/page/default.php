74894ed5d07fc17b9733607d38d7736b2d265806
Christian Francisco <christian.francisco@wsu.edu>
2018-02-05 00:15:11 -0800

    Converted SparqEdu template in source to SparqEdu template package

---
+++ packages/SparqEdu/src/templates/sparqedu/html/com_pages/page/default.php
@@ -0,0 +1,16 @@
+<? defined('KOOWA') or die('Restricted access'); ?>
+
+<? @title('SparqEdu - Home'); ?>
+<? @description('Welcome to SparqEdu!'); ?>
+
+<div class="row">
+	<div class="span8 offset2">
+		<div class="hero-unit text-center">
+			<img src="/templates/SparqEdu/css/SparqEduStyle/images/home.png"/>
+
+			<h1>SparqEdu</h1>
+
+			<p class="lead">Inspiration Powered by Connection.</p>
+		</div>
+	</div>
+</div>
