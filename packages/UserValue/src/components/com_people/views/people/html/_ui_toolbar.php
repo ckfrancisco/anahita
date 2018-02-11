0c3931f41e9d7bff2dd48e24d4c192c97e11d366
William <williamboston123@hotmail.com>
2018-02-11 01:11:05 -0800

    Add:  another seletion box? (not tested)

--- packages/UserValue/src/components/com_people/views/people/html/_ui_toolbar.php
+++ packages/UserValue/src/components/com_people/views/people/html/_ui_toolbar.php
@@ -18,18 +18,32 @@
         ComPeopleDomainEntityPerson::USERTYPE_ADMINISTRATOR => @text('COM-PEOPLE-USERTYPE-ADMINISTRATOR'),
         ComPeopleDomainEntityPerson::USERTYPE_SUPER_ADMINISTRATOR => @text('COM-PEOPLE-USERTYPE-SUPER-ADMINISTRATOR'),
     );
+    $html = $this->getService('com:base.template.helper.html');
+    ?>
+    
+    <?= $html->select('filter[usertype]', array('options' => $usertypes)) ?>
+    <label class="checkbox">
+        <input type="checkbox" name="filter[disabled]">
+        <?= @text('COM-PEOPLE-FILTER-DISABLED') ?>
+    </label>
+    <? endif; ?>
+</form>
 
-    $uservalue = array(
+<form action="<?= @route('layout=list') ?>" id="an-filterbox" class="an-filterbox form-inline" name="an-filterbox" method="get">
+    <input placeholder="Filter..." type="text" name="q" class="input-large search-query" id="an-search-query" value="" size="21" maxlength="100" />
+    <? if ($viewer->admin()) : ?>
+    <?
+    $uservalues = array(
         '' => AnTranslator::_('COM-PEOPLE-FILTER-USERVALUE'),
         ComPeopleDomainEntityPerson::USERVALUE_TEACHER => @text('COM-PEOPLE-USERVALUE-TEACHER'),
         ComPeopleDomainEntityPerson::USERVALUE_TUTOR => @text('COM-PEOPLE-USERVALUE-TUTOR'),
         ComPeopleDomainEntityPerson::USERVALUE_RECRUITER => @text('COM-PEOPLE-USERVALUE-RECRUITER'),
         ComPeopleDomainEntityPerson::USERVALUE_EMPLOYER => @text('COM-PEOPLE-USERVALUE-EMPLOYER'),
     );
-
     $html = $this->getService('com:base.template.helper.html');
     ?>
-    <?= $html->select('filter[usertype]', array('options' => $usertypes)) ?>
+    
+    <?= $html->select('filter[uservalue]', array('options' => $uservalues)) ?>
     <label class="checkbox">
         <input type="checkbox" name="filter[disabled]">
         <?= @text('COM-PEOPLE-FILTER-DISABLED') ?>
