451b62100cc04324f03d9585a6d9510b3404845c
William Boston <williamboston123@hotmail.com>
2018-04-02 13:17:19 -0700

    Add: types to the sql tables

---
+++ packages/SparqUserValues/src/componenets/com_sparqvalues/schemas/schema.sql
@@ -0,0 +1,5 @@
+ALTER TABLE '#__sessions' ADD `academictype` varchar(255);
+ALTER TABLE '#__sessions' ADD `corporatetype` varchar(255);
+
+ALTER TABLE '#__people_people' ADD `academictype` varchar(255);
+ALTER TABLE '#__people_people' ADD `corporatetype` varchar(255);
