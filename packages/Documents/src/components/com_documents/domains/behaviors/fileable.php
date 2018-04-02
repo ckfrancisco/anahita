c64ed2b1c64018d256f078c43f3b07c52afe4de3
Peter <peter.qafoku@wsu.edu>
2018-04-02 13:10:29 -0700

    ADD: tryed removing abstraction from fileable.php

--- packages/Documents/src/components/com_documents/domains/behaviors/fileable.php
+++ packages/Documents/src/components/com_documents/domains/behaviors/fileable.php
@@ -1,13 +1,20 @@
 <?php
 
 //class ComDocumentsDomainBehaviorFileable extends LibBaseDomainBehaviorFileable {
-class ComDocumentsDomainBehaviorFileable extends LibBaseDomainBehaviorStorable {
+//class ComDocumentsDomainBehaviorFileable extends LibBaseDomainBehaviorStorable {
+class ComDocumentsDomainBehaviorFileable extends AnDomainBehaviorAbstract {
 
   protected function _initialize(KConfig $config) {
-     parent::initialize($config);
-    syslog(1,"init complete");
 
-    unset($config['attributes']);
+    //remove this if we end up going back to LibBaseDomainBehaviorStorable
+    $config->append(array(
+         'storage' => $this->getService('plg:storage.default'),
+    ));
+
+    parent::initialize($config);
+    //syslog(1,"init complete");
+
+     unset($config['attributes']);
     $config->append(array (
       'attributes' => array (
           'filename',
@@ -16,12 +23,12 @@ class ComDocumentsDomainBehaviorFileable extends LibBaseDomainBehaviorStorable {
               'type' => 'integer',
               'write' => 'private'
           ),
-          'mimeType' => array(
-              'column' => 'medium_mime_type',
-              'match' => '/\w+\/\w+/',
-              'write' => 'private'
-          ),        )
-      ));
+         'mimeType' => array(
+               'column' => 'medium_mime_type',
+               'match' => '/\w+\/\w+/',
+               'write' => 'private'
+           ),        )
+       ));
   }
 
   public function getStoragePath($path = '') {
@@ -43,9 +50,9 @@ class ComDocumentsDomainBehaviorFileable extends LibBaseDomainBehaviorStorable {
     }
   }
 
-  protected function _beforeEntityDelete(KCommandContext $context) {
-    $this->deletePath($this->filename, false);
-  }
+  // protected function _beforeEntityDelete(KCommandContext $context) {
+  //   $this->deletePath($this->filename, false);
+  // }
 
   /**
    * Store Data.
@@ -92,4 +99,127 @@ class ComDocumentsDomainBehaviorFileable extends LibBaseDomainBehaviorStorable {
   {
       return $this->getValue('file_name');
   }
+//this is stuff from libBaseDomainBehaviorStorable, to remove later if needed
+
+/**
+ * Return the storage path of an entity. If $path is passed, it will
+ * append the $path to the base storage path.
+ *
+ * @param string $path The path to append the storage path with
+ *
+ * @return string
+ */
+public function getStoragePath($path = '')
+{
+    //prepend the path with a \/
+    if (strlen($path)) {
+        $path = '/'.$path;
+    }
+
+    $base = $this->_mixer->id;
+
+    //for ownable entities, use the owner component to prefix
+    //the path
+    if ($this->_mixer->isOwnable()) {
+        $path = '/'.$this->_mixer->component.$path;
+        $base = $this->_mixer->owner->id;
+    }
+
+    return 'n'.$base.$path;
+}
+
+/**
+ * Write data to entity storage.
+ *
+ * @param string $path   The relative path to store the data in
+ * @param string $data   The data to store
+ * @param bool   $public The storage mode. Can be public or private
+ */
+public function writeData($path = '', $data, $public = true)
+{
+    $path = $this->getStoragePath($path);
+
+    return $this->_storage->write($path, $data, $public);
+}
+
+/**
+ * Read data from entity storage.
+ *
+ * @param string $path   The relative path to read the data from
+ * @param bool   $public The storage mode. Can be public or private
+ *
+ * @return string
+ */
+public function readData($path = '', $public = true)
+{
+    $path = $this->getStoragePath($path);
+
+    return $this->_storage->read($path, $public);
+}
+
+/**
+ * Delete an existing data with the path.
+ *
+ * @param string $path   The relative path to delete the data from
+ * @param bool   $public The storage mode. Can be public or private
+ */
+public function deletePath($path = '', $public = true)
+{
+    $path = $this->getStoragePath($path);
+
+    return $this->_storage->delete($path, $public);
+}
+
+/**
+ * Checks the existance of path.
+ *
+ * @param string $path   The relative path to check
+ * @param bool   $public The storage mode. Can be public or private
+ *
+ * @return bool
+ */
+public function pathExists($path = '', $public = true)
+{
+    $path = $this->getStoragePath($path);
+
+    return $this->_storage->exists($path, $public);
+}
+
+/**
+ * Gets the unique identifiable location (URL) for a given path.
+ *
+ * @param string $path   The relative path to check
+ * @param bool   $public The storage mode. Can be public or private
+ *
+ * @return bool
+ */
+public function getPathURL($path = '', $public = true)
+{
+    $path = $this->getStoragePath($path);
+
+    return $this->_storage->getUrl($path, $public);
+}
+
+/**
+ * Completely remove the storage.
+ */
+public function removeStorage()
+{
+    $this->deletePath('', true);
+    $this->deletePath('', false);
+}
+
+/**
+ * Before delete command.
+ *
+ * When an entity is deleted, the call of this command removes the deleted entity storage
+ *
+ * @param KCommandContext $context Context parameter
+ */
+protected function _beforeEntityDelete(KCommandContext $context)
+{
+    $entity = $context->entity;
+    $entity->removeStorage();
+}
+
 }
