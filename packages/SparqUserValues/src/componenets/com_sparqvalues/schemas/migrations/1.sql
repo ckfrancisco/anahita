c7504f1711f2d4f382208554a24cf6852317ee74
William Boston <williamboston123@hotmail.com>
2018-04-02 16:30:50 -0700

    copied into this just cuz

--- packages/SparqUserValues/src/componenets/com_sparqvalues/schemas/migrations/1.sql
+++ packages/SparqUserValues/src/componenets/com_sparqvalues/schemas/migrations/1.sql
@@ -1,5 +1,7 @@
-ALTER TABLE '#__sessions' ADD `academictype` varchar(255);
-ALTER TABLE '#__sessions' ADD `corporatetype` varchar(255);
+--------------------------------------------------------
 
-ALTER TABLE '#__people_people' ADD `academictype` varchar(255);
-ALTER TABLE '#__people_people' ADD `corporatetype` varchar(255);
+ALTER TABLE '#__sessions' ADD 'academictype' varchar(255);
+ALTER TABLE '#__sessions' ADD 'corporatetype' varchar(255);
+
+ALTER TABLE '#__people_people' ADD 'academictype' varchar(255);
+ALTER TABLE '#__people_people' ADD 'corporatetype' varchar(255);
