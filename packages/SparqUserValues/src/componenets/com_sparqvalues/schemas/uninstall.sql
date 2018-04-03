786b6e0f3d54873fa343a6f70c05f7dd3b9a38f4
William Boston <williamboston123@hotmail.com>
2018-04-02 20:48:43 -0700

    Remove: removed stuff after renaming the folder

--- packages/SparqUserValues/src/componenets/com_sparqvalues/schemas/uninstall.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE `#__sessions` DROP `academictype` varchar(255);
-ALTER TABLE `#__sessions` DROP `corporatetype` varchar(255);
-
-ALTER TABLE `#__people_people` DROP `academictype` varchar(255);
-ALTER TABLE `#__people_people` DROP `corporatetype` varchar(255);
