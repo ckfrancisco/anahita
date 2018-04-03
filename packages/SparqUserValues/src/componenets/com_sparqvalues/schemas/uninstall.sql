a0e99a1b7e9f6a40f13fca4449a4ef81c9250899
William Boston <williamboston123@hotmail.com>
2018-04-02 18:24:17 -0700

    Edit: removed 1.sql and added the uninstall.sql

---
+++ packages/SparqUserValues/src/componenets/com_sparqvalues/schemas/uninstall.sql
@@ -0,0 +1,5 @@
+ALTER TABLE `#__sessions` DROP `academictype` varchar(255);
+ALTER TABLE `#__sessions` DROP `corporatetype` varchar(255);
+
+ALTER TABLE `#__people_people` DROP `academictype` varchar(255);
+ALTER TABLE `#__people_people` DROP `corporatetype` varchar(255);
