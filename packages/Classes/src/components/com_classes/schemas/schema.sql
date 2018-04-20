47511059e4b5f1567941cb50526ca8e3860f1f51
Jerdon Helgeson <jerdonhelgeson@gmail.com>
2018-04-20 10:44:07 -0700

    packages reverted to working state

--- packages/Classes/src/components/com_classes/schemas/schema.sql
+++ packages/Classes/src/components/com_classes/schemas/schema.sql
@@ -5,10 +5,10 @@ CREATE TABLE `#__classes_classes` (
   `node_id` bigint(11) NOT NULL,
   `class` varchar(50) DEFAULT NULL,
   `people_person_id` bigint(11) NOT NULL,
-  /*`open_status_change_time` datetime NOT NULL,
-  `open_status_change_by` bigint(11) DEFAULT NULL,*//*--Jerdon Helgeson--*/
+  `open_status_change_time` datetime NOT NULL,
+  `open_status_change_by` bigint(11) DEFAULT NULL,
   PRIMARY KEY (`classes_classes_id`),
   UNIQUE KEY `node_id` (`node_id`)
 ) ENGINE=InnoDB;
 
-INSERT INTO #__migrator_versions (`version`,`component`) VALUES(4, 'classes') ON DUPLICATE KEY UPDATE `version` = 4; /*--Jerdon Helgeson--*/
+INSERT INTO #__migrator_versions (`version`,`component`) VALUES(6, 'classes') ON DUPLICATE KEY UPDATE `version` = 4; /*--Jerdon Helgeson--*/
